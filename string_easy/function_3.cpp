2502f13f263f0d658b9a383dae6fd85a8944c487
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-26 14:34:57 +0300

    Update function_3.cpp

--- string_easy/function_3.cpp
+++ string_easy/function_3.cpp
@@ -17,8 +17,8 @@ string itc_cmp_str(string str1, string str2, int num){
         str = str + str2[k];
     }
     if (i + k < len1){
-        for (p = i + 1; p + i < len1; p++){
-                str = str + str1[p];
+        for (p = i + 1; p + i < len1 - len2 + 1; p++){
+            str = str + str1[p];
         }
     }
     return str;
