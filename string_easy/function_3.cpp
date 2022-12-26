37618ef9ab2feb722f7a083b9f3309e8cd52dd14
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-26 21:28:36 +0300

    Update function_3.cpp

--- string_easy/function_3.cpp
+++ string_easy/function_3.cpp
@@ -24,4 +24,23 @@ string itc_cmp_str(string str1, string str2, int num){
     return str;
 }
 
-
+int itc_find_str(string str1, string str2){
+    long long i = 0;
+    long long p = 0;
+    long long kol = 0;
+    long long len1 = itc_len(str1);
+    long long len2 = itc_len(str2);
+    for (i = 0; i < len1; i++){
+        if (str1[i] == str2[0]){
+            for (p = 0; p < len2; p++){
+                if (str1[i+p] == str2[p]){
+                    kol++;
+                }
+            }
+        }
+        if (kol == len2){
+            return i;
+        }
+    }
+    return -1;
+}
