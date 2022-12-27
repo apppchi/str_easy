e0f8bddd888cbec908baa70d9d73ef42cfe6d467
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-27 22:08:28 +0300

    Update function_3.cpp

--- string_easy/function_3.cpp
+++ string_easy/function_3.cpp
@@ -32,6 +32,7 @@ int itc_find_str(string str1, string str2){
     long long len2 = itc_len(str2);
     for (i = 0; i < len1; i++){
         if (str1[i] == str2[0]){
+            kol = 0;
             for (p = 0; p < len2; p++){
                 if (str1[i+p] == str2[p]){
                     kol++;
